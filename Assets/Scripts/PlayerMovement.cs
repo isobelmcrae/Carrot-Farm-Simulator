32e4977f699fe1cb8cbde7aed0aed9ac9c8d115e
isobel mcrae <97225201+isobelmcrae@users.noreply.github.com>
2023-09-01 08:51:48 +0930

    fixed hoe anim

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Tilemaps;
 using UnityEngine;
-using TMPro.Examples;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
