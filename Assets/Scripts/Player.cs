105b5e3a58c7bdd1165b1a78ef9f67c8ed69f793
isobel mcrae <97225201+isobelmcrae@users.noreply.github.com>
2023-09-01 18:19:42 +0930

    more farming

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -66,30 +66,34 @@ public class Player : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Mouse0) && !isUsing) 
         {
             UsingCD(); //triggers cooldown before player can input again
-            if(inventoryManager.GetSelectedItem(false).name == "Hoe") 
+            if(inventoryManager.GetSelectedItem(false).name == "Hoe" /* && tile is interactable */) 
             {
                 Vector3 point = new Vector3();
+                Vector3 pointPos = new Vector3();
+
+                pointPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane);
 
                 // converts mouse position to world position
-                point = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
+                point = cam.ScreenToWorldPoint(pointPos);
                 // converts world position to cell position on the grid
                 Vector3Int cellPosition = grid.WorldToCell(point);
-
-                // if the dictionary has no values, add the current cell position and instantiate a farming space
-                if(activeTiles.Count == 0) {
-                    activeTiles.Add(cellPosition, "tilled");
-                    Instantiate(farmingSpacePrefab, grid.GetCellCenterWorld(cellPosition), Quaternion.identity);
-                    // only plays animation if farming space is created
-                    animator.Play("PlayerHoe");
-                } else {
-                    // if the dictionary has values, check if the current cell position is already in the dictionary
-                    if(!activeTiles.ContainsKey(cellPosition))  {
+                if (Vector3.Distance(pointPos, cellPosition) > 20f) {
+                    // if the dictionary has no values, add the current cell position and instantiate a farming space
+                    if(activeTiles.Count == 0) {
                         activeTiles.Add(cellPosition, "tilled");
                         Instantiate(farmingSpacePrefab, grid.GetCellCenterWorld(cellPosition), Quaternion.identity);
                         // only plays animation if farming space is created
                         animator.Play("PlayerHoe");
                     } else {
-                        return;
+                        // if the dictionary has values, check if the current cell position is already in the dictionary
+                        if(!activeTiles.ContainsKey(cellPosition))  {
+                            activeTiles.Add(cellPosition, "tilled");
+                            Instantiate(farmingSpacePrefab, grid.GetCellCenterWorld(cellPosition), Quaternion.identity);
+                            // only plays animation if farming space is created
+                            animator.Play("PlayerHoe");
+                        } else {
+                            return;
+                        }
                     }
                 }
                 
